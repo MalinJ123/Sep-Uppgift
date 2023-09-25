90cd3a8914ff4b6a96c1b1c2a6f7dff2dc3465c7
Malin123 <malin--jonsson@hotmail.com>
2023-09-25 21:10:23 +0200

    dagens arbete

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
-import './App.css';
-import AWS from 'aws-sdk';
+import React, { useState } from "react";
+import "./App.css";
+import AWS from "aws-sdk";
+import axios from "axios";
 
 function App() {
-
   const [selectedFile, setSelectedFile] = useState(null);
   const [images, setImages] = useState([]);
+  const [imageUrl, setImageUrl] = useState("");
+  const [processedImageUrl, setProcessedImageUrl] = useState("");
 
-  
-AWS.config.update({ 
-  accessKeyId: '', 
-  secretAccessKey: '', 
-  region: '', 
-  }); 
+ 
 
-const s3 = new AWS.S3();
+  const s3 = new AWS.S3();
+
+  //post api - lambda post funktion API GateWay? 
+  const handleProcessImage = async () => {
+    try {
+      const response = await axios.post(
+        "https://1arzdoqt8f.execute-api.eu-north-1.amazonaws.com/dev/",
+        { body: imageUrl }
+      );
+      setProcessedImageUrl(response.data);
+      setImageUrl("");
+    } catch (error) {
+      console.error("Error processing image:", error);
+    }
+  };
+
+
 
   const handleUpload = () => {
     if (!selectedFile) {
@@ -38,7 +51,7 @@ const s3 = new AWS.S3();
       } else {
         console.log("Bilden laddades upp!:", data.Location);
 
-        getImages();
+        // getImages();
       }
     });
   };
@@ -47,6 +60,19 @@ const s3 = new AWS.S3();
     setSelectedFile(event.target.files[0]);
   };
 
+  const getImages = () => {
+    const params = {
+      Bucket: "save-animal-pic",
+    };
+    s3.listObjectsV2(params, (err, data) => {
+      if (err) {
+        console.error("Fel vid hämtning av bilder:", err);
+      } else {
+        const imageList = data.Contents.map((obj) => obj.Key);
+        setImages(imageList);
+      }
+    });
+  };
 
   return (
     <>
@@ -54,9 +80,35 @@ const s3 = new AWS.S3();
       <div className="input-div">
         <input type="file" onChange={handleFileChange} />
       </div>
-      <button className="upload-btn" onClick={handleUpload}>
+      <button className="upload btn" onClick={handleUpload}>
         Lägg till bilder
       </button>
+      <button className="show btn" onClick={getImages}>
+        Visa bilder
+      </button>{" "}
+      <div className="image-list">
+        {images.map((imageName, index) => (
+          <img
+            key={index}
+            src={`https://save-animal-pic.s3.eu-north-1.amazonaws.com/${imageName}`}
+            alt={imageName}
+          />
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+          placeholder="Enter image URL"
+          />
+        <button onClick={handleProcessImage}>Process Image</button>
+        {processedImageUrl && (
+          <div>
+            <img src={processedImageUrl} alt="Processed Image" />
+          </div>
+        )}
+      </div>
     </>
   );
 }
